265f791c92bc047c5e709106f29e0dcee52632fc
Satyam Mahajan <mahajansatyam2003@gmail.com>
2025-02-24 23:20:37 +0530

    Initial commit

---
+++ expense-front/src/Components/LoginComponent/CustomerMenu.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const CustomerMenu=()=>{
+
+    return (
+        <h2>
+        This is Register page
+        </h2>
+    );
+}
+export default CustomerMenu;
