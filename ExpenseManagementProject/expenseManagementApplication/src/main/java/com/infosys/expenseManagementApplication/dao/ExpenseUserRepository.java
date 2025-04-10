1eaf8ff7ba922b1c98cdce757637afc41b3ed290
Satyam Mahajan <mahajansatyam2003@gmail.com>
2025-04-10 17:01:21 +0530

    Added Expense Presentation file

--- ExpenseManagementProject/expenseManagementApplication/src/main/java/com/infosys/expenseManagementApplication/dao/ExpenseUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.expenseManagementApplication.dao;
-
-import com.infosys.expenseManagementApplication.bean.*;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseUserRepository extends JpaRepository<ExpenseUser,String> {
-
-}
